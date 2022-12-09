01cb8273d807dc2f6635e90af950c1597b0d6d91
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-09 01:01:08 +0100

    lets go there!

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char fifthTask[] = "with proper grammar, but the outcome is a piece of art,";
+
+printf("character = %s", fifthTask);
+  
+return (0);
+}
