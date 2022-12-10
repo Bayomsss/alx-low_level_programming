2d8ed9994a76606f71e9104c8fe09bd40212d27e
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-10 05:45:26 +0100

    this c code will run a predefined output based on a random generated input

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+
 /**
+ * this code runs a predefined output (negative, zero, or positive) based on a random generated input.
  * main - Entry point
- * Bayomsss Doc created 10 Dec, 2022 by 05:06
- * this file takes an input and returns negative, zero or positive based on the specified input
+ * created by Bayomss 10 Dec, 2022 at 05:38am
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-        if (n > 0){
-	printf("Positive");
-	}
-	else if (n == 0){
-	printf("Zero");
-	}
-	else{
-	  printf("Negative");
-	}
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0){
+printf("is positive");
+}
+else if (n == 0){
+printf("is zero");
+}
+else
+printf("is negative");
+return (0);
 }
