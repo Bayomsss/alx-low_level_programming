8a40b5c1c6622c9ca780c02dc338960bafb3dd0d
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-11 07:47:40 +0100

    testing the script i just created

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * this program prints the alphabet in lowercase using the putchar function
+ * Bayomsss 10 Dec at 07:31 am
+ * Return: Always 0 (Success)
+ */
+char n[]= a-z;
+
+int main(void)
+{
+  putchar("%c", n);
+return (0);
+}
