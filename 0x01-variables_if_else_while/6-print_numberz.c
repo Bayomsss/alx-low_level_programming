90a9645bb4d000184badfe6179fbb0320ed581e5
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-13 02:10:03 +0100

    print alphabet in reverse

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * created by Bayomi on 13 Dec 01:30
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int singleDigit;
+
+for (singleDigit = 0; singleDigit < 10; singleDigit++)
+
+putchar(singleDigit);
+
+putchar("\n");
+
+return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * created by Bayomsss 13 Dec 02:02
+ * prints lowercase alphabet in reverse
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int alphabet;
+
+for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+{
+putchar(alphabet);
+}
+}
