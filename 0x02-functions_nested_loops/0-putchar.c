622b16dd3cd0f583c7b59ecc9aa158903cc7139d
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-14 05:48:36 +0100

    omo

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print a given output
+ * created by Bayomi, 14 Dec, 05:12
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char *s="_putchar\n";
+while (*s) putchar(*s++);
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project is about functions and nested loops.
