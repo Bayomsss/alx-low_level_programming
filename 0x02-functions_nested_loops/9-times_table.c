e063922f7db797e781ca8bc57303b835db4ee634
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-15 16:13:59 +0100

    this is loop

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,18 +12,34 @@ void times_table(void)
 
 int r, c, d;
 
-for (r = 0; r <= 9; r++)
+for (r = 0; r < 10; r++)
 {
-_putchar('0');
+
+for (c = 0; c < 10; c++)
+{
+
+d = c * r;
+
+if (c == 0)
+{
+_putchar(d + '0');
+}
+if (d < 10 && c != 0)
+{
 _putchar(',');
 _putchar(' ');
-for (c = 1; c <= 9; c++)
+_putchar(' ');
+_putchar(d + '0');
+}
+else if (d >= 10)
 {
-d = (r * c);
-if ((d / 10) > 0)
-{
+_putchar(',');
+_putchar(',');
 _putchar((d / 10) + '0');
+_putchar((d % 10) + '0');
+}
+_putchar('\n');
 }
 }
 }
-}
+
