9ae610339e93efdde98354949a411b15d2e3e42f
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-15 06:40:54 +0100

    jj

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+/**
+ * print_sign - Entry point
+ * 06:29am 15th Dec by Bayomss
+ * @n: argument
+ * Return: 0 and 1 success
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar (+);
+return (1);
+}
+else if (n = 0)
+{
+_putchar (0);
+return (0); 
+}
+else
+{
+_putchar (-);
+return (-1);
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,3 +7,5 @@ int _putchar(char);
 int _islower(int c);
 
 int _isalpha(int c);
+
+int print_sign(int n);
