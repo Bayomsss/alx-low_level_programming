1c6e41d0f3faf3931bc249b6181d583294bbeb1e
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-15 07:48:01 +0100

    io

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - computes absolute value of integer
+ * 07:37am 15th Dec by Bayomsss
+ * Return: Always 0
+ */
+
+int _abs(int)
+{
+int x;
+int abs (x);
+
+}
