a1d419177a1bd6163b62a8d1980b3494d6bb3e2e
Bayomsss <darabayo.bd@gmail.com>
2022-12-14 21:19:29 -0800

    jj

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+/**
+ * _isalpha - Entry point
+ * 06:11am 15th Dec by Bayomsss
+ * @c: argument
+ * int: data type
+ * Return: 0 and 1 success
+ */
+
+int _isalpha(int c)
+{
+  return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,3 +5,5 @@ void print_alphabet_x10(void);
 int _putchar(char);
 
 int _islower(int c);
+
+int _isalpha(int c);
