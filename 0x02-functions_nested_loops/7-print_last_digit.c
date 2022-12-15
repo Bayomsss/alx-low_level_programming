3593cdfe24ef8f53199faab64c5553da63b650ae
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-15 10:51:22 +0100

    just chilling

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,14 @@
 /**
  * print_last_digit - Entry point
  * created 15 Dec 2022 by 10:11am
+ * @l: last digit
  * @n: number in question
  * Return: value of last digit
  */
 
 int print_last_digit(int n)
 {
-return (n);
+int l;
+l = n % 10;
+return (l);
 }
