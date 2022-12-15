56e3e2904b28986d4a18cb8178b95efe2ef2b4eb
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-15 10:21:36 +0100

    this works?

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * created 15 Dec 2022 by 10:11am
+ * @n: number in question
+ * @l: last digit in question
+ * Return: value of last digit
+ */
+
+int print_last_digit(int n, int l)
+{
+
+l = n % 10;
+return (n);
+}
+
