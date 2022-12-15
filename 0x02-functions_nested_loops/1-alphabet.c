ae78540a0930ecfd7ab1cab5b4c4afc8d80101db
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-15 01:45:21 +0100

    what's happening here

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "alphabet.h"
+
+/**
+ * main - check the code
+ * created by Bayomsss, 15th Dec, 2022 at 01:36am
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+int i;
+print_alphabet(i);
+putchar(i);
+return (0);
+}
