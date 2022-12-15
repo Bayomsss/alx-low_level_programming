55cdae0d0150e69202ffbc8314b662172739e188
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-15 11:33:02 +0100

    na wa oo

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * print_last_digit - Entry point
+ * print_last_digit - Prints the last digit of a number.
  * created 15 Dec 2022 by 10:11am
- * @l: last digit
  * @n: number in question
  * Return: value of last digit
  */
@@ -11,6 +10,9 @@ int print_last_digit(int n)
 {
 int l;
 l = n % 10;
-_putchar (l);
+
+if(l < 0)
+l *= -1;
+_putchar(l + '0');
 return (l);
 }
