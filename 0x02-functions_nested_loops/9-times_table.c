cf9eddcd000a6840735fc968bf93adeddeb08bce
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-15 15:17:21 +0100

    hkkj

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ * r = row, c = column, d = current result
+ * Returns: times tables
+ * add extra space past single digit
+ */
+
+void times_table(void)
+{
+
+int r, c, d;
+
+for (r = 0; r <= 9; r++)
+{
+_putchar('0');
+_putchar(',');
+_putchar(' ');
+for (c = 1; c <= 9; c++)
+{
+d = (r * c);
+if ((d / 10) > 0)
+{
+_putchar((d / 10) + '0');
+}
+}
+}
+}
