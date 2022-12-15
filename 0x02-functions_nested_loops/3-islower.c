733e90bb1442b7eee7cb9792db92a639a5193875
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-15 05:43:46 +0100

    jfda

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,9 @@
 #include <ctype.h>
 
 /**
- * _islower(int c) - Entry point
+ * _islower(int c) - checks for lower case letter
  * 04:07am 15th Dec by Bayomsss
- * Return: Always 0.
+ * Return: returns 0 or 1 depending on condition
  */
 
 int _islower(int c)
