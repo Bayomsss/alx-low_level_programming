41be9a3e2e9ee2080fe1af89706dc241458b092e
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-17 04:51:33 +0100

    nested is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ * 04:49am 17th Dec by Bayomsss
+ * @c: argument
+ * int: data type
+ * Return: 0 and 1 success
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
