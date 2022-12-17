2a528fdfad84b0c6ad79fd26d81c3186d5be9ea8
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-17 05:19:05 +0100

    hmm

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * 05am 17th Dec by Bayomsss
+ * @c: argument
+ * int: data type
+ * Return: 0 and 1 success
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
 int _isupper(int c);
 
 int _isdigit(int c);
+
+int mul(int a, int b);
+int _putchar(char);
