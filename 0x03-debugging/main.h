4e7fb31ec8b627815909c70d98bf3d29441d0e27
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-17 04:03:55 +0100

    now maybe

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,7 +9,7 @@ int main(void);
 
 int largest_number(int a, int b, int c);
 
-int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
