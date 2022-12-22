0f74155a0c8633ef2d3157345de0553a32fbd1d7
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-22 14:09:58 +0100

    joe

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: character
+ * Bayomsss created 13:53pm
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+int length = 0;
+
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return length;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ void reset_to_98(int *n);
 int _putchar(int);
 
 void swap_int(int *a, int *b);
+int _putchar(int);
